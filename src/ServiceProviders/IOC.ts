682ad62a65917e03943679477fbdaf6055c40424
Igor Kov <igor875126@gmail.com>
2021-01-21 13:57:38 +0600

    IOC class added

---
+++ src/ServiceProviders/IOC.ts
@@ -0,0 +1,19 @@
+import IOCMakeException from '../Exceptions/IOC/IOCMakeException';
+
+export default class IOC {
+    /**
+     * Get object from IOC
+     */
+    public static make(name: any): any {
+        // Require ioc config
+        const iocConfig = require(process.cwd() + '/build/config/ioc').default;
+
+        // Check if requested name exists in ioc.ts config
+        if (iocConfig.normal[name]) {
+            return iocConfig.normal[name]();
+        }
+
+        // Nope requested name does not exists in ioc.ts config
+        throw new IOCMakeException('IOC could not resolve class, please check IOC.ts and register needed name there', name);
+    }
+}
