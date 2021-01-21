b446ccf437097335707eb54305b52540402d3053
dan <dangavrilin@gmail.com>
2021-01-21 14:06:36 +0600

    Add debug and critical log levels Show pretty logs in console while developing

---
+++ src/Repositories/Logger/LoggerPrettyConsoleRepository.ts
@@ -0,0 +1,65 @@
+import dayjs from 'dayjs';
+import colors from 'colors';
+import LoggerRepositoryInterface from './LoggerRepositoryInterface';
+
+export default class LoggerPrettyConsoleRepository implements LoggerRepositoryInterface {
+    /**
+     * Log info message
+     */
+    public debug(message: string, params: any): void {
+        console.log(this.makePrettyString('debug', message, params));
+    }
+
+    /**
+     * Log info message
+     */
+    public info(message: string, params: any): void {
+        console.log(this.makePrettyString('info', message, params));
+    }
+
+    /**
+     * Log warning message
+     */
+    public warning(message: string, params: any): void {
+        console.log(this.makePrettyString('warning', message, params));
+    }
+
+    /**
+     * Log error message
+     */
+    public error(message: string, params: any): void {
+        console.log(this.makePrettyString('error', message, params));
+    }
+
+    /**
+     * Log critical message
+     */
+    public critical(message: string, params: any): void {
+        console.log(this.makePrettyString('critical', message, params));
+    }
+
+    /**
+     * Compose and return a pretty string for logger
+     */
+    private makePrettyString(level: 'debug' | 'info' | 'warning' | 'error' | 'critical', message: string, params: any = null): string {
+
+        const levelString: string = (() => {
+            switch (level) {
+                case 'debug':
+                    return colors.magenta('DEBUG');
+                case 'info':
+                    return colors.green('INFO ');
+                case 'warning':
+                    return colors.yellow('WARN ');
+                case 'error':
+                    return colors.red('ERROR');
+                case 'critical':
+                    return colors.bgRed('CRIT ');
+            }
+        })();
+
+        const output: string = `${colors.dim(dayjs().format('YYYY-MM-DD HH:mm:ss'))} ${levelString} ${message} ${(params !== null) ? colors.cyan(params) : ''}`;
+
+        return output;
+    }
+}
