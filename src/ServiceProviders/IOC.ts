b5edb9090c2c3b346584c0e3fc2f4263718bb9a5
Igor Kov <igor875126@gmail.com>
2021-01-22 07:30:35 +0600

    IOC now has out of the box core services registered

--- src/ServiceProviders/IOC.ts
+++ src/ServiceProviders/IOC.ts
@@ -1,6 +1,31 @@
 import IOCMakeException from '../Exceptions/IOC/IOCMakeException';
+import LoggerJsonConsoleRepository from '../Repositories/Logger/LoggerJsonConsoleRepository';
+import LoggerPrettyConsoleRepository from '../Repositories/Logger/LoggerPrettyConsoleRepository';
+import LoggerService from '../Services/Logger/LoggerService';
 
 export default class IOC {
+    /**
+     * Core services are services what sosise-core provides out of the box.
+     * When user tries to get instance with make method the order is the following:
+     * 1. IOC tries to find the name in user defined config ioc.ts
+     * 2. If the first try fails, IOC tries to find the name in the coreServices
+     * 3. When the first and second steps fails exception will be raised
+     */
+    private static coreServices = {
+        // Normal means, that these registrations are non-singletons (sigletons are not supported atm.)
+        normal: {
+            /**
+             * LoggerService
+             */
+            LoggerService: () => {
+                if (process.env.APP_ENV === 'local') {
+                    return new LoggerService(new LoggerPrettyConsoleRepository());
+                }
+                return new LoggerService(new LoggerJsonConsoleRepository());
+            }
+        },
+    };
+
     /**
      * Get object from IOC
      */
@@ -11,6 +36,11 @@ export default class IOC {
         // Check if requested name exists in ioc.ts config
         if (iocConfig.normal[name]) {
             return iocConfig.normal[name]();
+        }
+
+        // Check if requested name exists in local coreServices property
+        if (this.coreServices.normal[name]) {
+            return this.coreServices.normal[name]();
         }
 
         // Nope requested name does not exists in ioc.ts config
