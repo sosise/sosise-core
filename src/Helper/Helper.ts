68a2e70b54a5683af8bb4c03cd67172e230d4d5f
Igor Kov <igor875126@gmail.com>
2022-11-17 11:30:32 +0600

    Helper methods added

--- src/Helper/Helper.ts
+++ src/Helper/Helper.ts
@@ -3,7 +3,8 @@ import { inspect } from 'util';
 
 export default class Helper {
     /**
-     * Helper method which checks if given date is not null, casts to date format
+     * Check if given date is not null, casts to date format
+     * @return YYYY-MM-DD
      */
     public static parseDate(date: string): string | null {
         if (date === null || date.toString().trim() === '') {
@@ -14,7 +15,8 @@ export default class Helper {
     }
 
     /**
-     * Helper method which checks if given date is not null, casts to date format
+     * Check if given date time is not null, casts to date time format
+     * @return YYYY-MM-DD HH:mm:ss
      */
     public static parseDateTime(date: string): string | null {
         if (date === null || date.toString().trim() === '') {
@@ -25,7 +27,8 @@ export default class Helper {
     }
 
     /**
-     * Helper method which gets current date time
+     * Get current date time
+     * @return YYYY-MM-DD HH:mm:ss
      */
     public static getCurrentDateTime(): string {
         return dayjs().format('YYYY-MM-DD HH:mm:ss');
@@ -67,7 +70,7 @@ export default class Helper {
     }
 
     /**
-     * Sleep amount of seconds
+     * Sleep amount of milliseconds
      */
     public static sleep(milliseconds: number): Promise<void> {
         return new Promise((resolve) => setTimeout(resolve, milliseconds));
@@ -75,7 +78,7 @@ export default class Helper {
 
     /**
      * Path to the project with ending slash
-     * e.g. /tmp/myproject/
+     * @return e.g. /tmp/myproject/
      */
     public static projectPath(): string {
         return process.cwd() + '/';
@@ -83,9 +86,88 @@ export default class Helper {
 
     /**
      * Path to the storage with ending slash
-     * e.g. /tmp/myproject/storage/
+     * @return e.g. /tmp/myproject/storage/
      */
     public static storagePath(): string {
         return process.cwd() + '/storage/';
+    }
+
+    /**
+     * Pluck fields from object or from array of objects
+     * @return object or array with needed fields
+     *
+     * @example
+     * Helper.pluckMany([{name: 'alex', age: 10}, {name: 'sharon', age: 11}], ['age']) -> [{age: 10}, {age: 11}]
+     *
+     * @example
+     * Helper.pluckMany({name: 'igor', age: 33, birthday: 'foobar'}, ['age', 'birthday']) -> {age: 33, birthday: 'foobar'}
+     */
+    public static pluckMany(collection: unknown, fields: string[]): unknown {
+        if (collection instanceof Array) {
+            const result: any[] = [];
+            for (const element of collection) {
+                const tmp: any = {};
+                for (const field of fields) {
+                    if (element[field] !== undefined) {
+                        tmp[field] = element[field];
+                    }
+                }
+                result.push(tmp);
+            }
+            return result;
+        }
+
+        if (collection instanceof Object) {
+            const result: any = {};
+            for (const field of fields) {
+                if (collection[field] !== undefined) {
+                    result[field] = collection[field];
+                }
+            }
+            return result;
+        }
+    }
+
+    /**
+     * Start profiling
+     * Creates a time measurement label
+     */
+    public static timeMeasurementLabel = 1;
+    public static startProfiling() {
+        console.time(Helper.timeMeasurementLabel.toString());
+    }
+
+    /**
+     * Stop profiling
+     * Displays past time after last startProfiling method call
+     */
+    public static stopProfiling() {
+        console.timeEnd(Helper.timeMeasurementLabel.toString());
+        Helper.timeMeasurementLabel++;
+    }
+
+    /**
+     * Paginate any array with data
+     * @return part of array on specific page and size
+     */
+    public static paginateArray(data: any[], page: number, pageSize: number): any[] {
+        return data.slice((page - 1) * pageSize, page * pageSize);
+    }
+
+    /**
+     * Assemble pagination
+     * @return pagination object
+     */
+    public static assemblePagination(data: any[], page: number, pageSize: number): { page: number, pageSize: number, totalPages: number, totalElements: number } {
+        // Create pagination object
+        const pagination: { page: number, pageSize: number, totalPages: number, totalElements: number } = {
+            page,
+            pageSize,
+            totalPages: Math.ceil(data.length / pageSize),
+            totalElements: data.length
+        };
+
+        // Return
+        return pagination;
     }
 }
